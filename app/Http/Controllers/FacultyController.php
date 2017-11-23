f89d7e95fc276f2e6a94c8fe600ee2eb2b0c3870
htonkovac <htonkovac@gmail.com>
2017-11-23 22:31:12 +0100

    admin middleware

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class FacultyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +18,7 @@ class FacultyController extends Controller
      */
     public function index()
     {
-        //
+        dd('hello');
     }
 
     /**
