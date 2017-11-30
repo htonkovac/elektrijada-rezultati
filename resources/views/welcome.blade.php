4f026319ecc5367ef41f704ba3be2c2f67b158bf
htonkovac <htonkovac@gmail.com>
2017-11-30 19:38:05 +0100

    Changed title to Stem Games

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Elektrijada Rezultati
+                    STEM games Rezultati
                 </div>
 
                 <div class="links">
